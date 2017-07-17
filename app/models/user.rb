e668d5293af7ae0826fd45f806bb1fb1afc2f861
Thai Viet <tbv.bkhn@gmail.com>
2017-07-17 11:00:37 +0700

    Make a basic User Model

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save :email_downcase
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+
+  private
+
+  def email_downcase
+    email.downcase!
+  end
+end
