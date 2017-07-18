c99662e92ea41d7efc074445e1ce389551d5ddd2
Thai Viet <tbv.bkhn@gmail.com>
2017-07-18 13:20:59 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
-  def new; end
+  attr_reader :user
+  def show
+    @user = User.find(params[:id])
+    return if user
+    flash[:danger] = t "not_exit_user"
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if user.save
+      flash[:success] = t "welcome_to_app"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
