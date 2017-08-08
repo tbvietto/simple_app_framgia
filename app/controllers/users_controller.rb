1cb777e285197c327bba55c2b62e650d2fbc636c
Thai Viet <tbv.bkhn@gmail.com>
2017-08-08 14:52:55 +0700

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if user.save
-      log_in user
-      flash[:success] = t "welcome_to_app"
-      redirect_to user
+      user.send_activation_email
+      flash[:info] = t ".check_email"
+      redirect_to root_url
     else
       render :new
     end
